b2d2c2366b04d9b9d23b8ce4114dd44c8c7fba11
Badia <bediyagrissa@gmail.com>
2024-08-04 08:38:37 +0100

    Adding SkateCharacter and Playground gamemode and bind the correct pawn class

---
+++ Source/SkatingSimulator/PlaygroundGamemode.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlaygroundGamemode.h"
+
+APlaygroundGamemode::APlaygroundGamemode()
+{
+	static ConstructorHelpers::FClassFinder<APawn> BP_SkateCharacter(TEXT("/Game/Blueprints/BP_SkateCharacter"));
+	if (BP_SkateCharacter.Class != NULL) {
+		DefaultPawnClass = BP_SkateCharacter.Class;
+	}
+	else
+		DefaultPawnClass = ASkateCharacter::StaticClass();
+}
---
+++ Source/SkatingSimulator/PlaygroundGamemode.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UObject/ConstructorHelpers.h"
+#include "SkateCharacter.h"
+#include "PlaygroundGamemode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKATINGSIMULATOR_API APlaygroundGamemode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APlaygroundGamemode();
+		
+};
