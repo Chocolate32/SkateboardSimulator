c5f899da7c615596489c5036172e109e04cb0c74
Badia <bediyagrissa@gmail.com>
2024-08-04 11:51:08 +0100

    Setting up Character base Components

--- Source/SkatingSimulator/SkateCharacter.cpp
+++ Source/SkatingSimulator/SkateCharacter.cpp
@@ -9,6 +9,16 @@ ASkateCharacter::ASkateCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	CameraArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Camera Arm"));
+	CameraArm->SetupAttachment(RootComponent);
+	CameraArm->TargetArmLength = TargetArmLength;
+	
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(CameraArm);
+
+	SkateMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Skate Mesh"));
+	SkateMesh->SetupAttachment(RootComponent);
+
 }
 
 // Called when the game starts or when spawned
--- Source/SkatingSimulator/SkateCharacter.h
+++ Source/SkatingSimulator/SkateCharacter.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
 #include "SkateCharacter.generated.h"
 
 UCLASS()
@@ -11,9 +13,22 @@ class SKATINGSIMULATOR_API ASkateCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* CameraArm;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* Camera;
+
+	UPROPERTY(EditAnywhere, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	float TargetArmLength{ 400.f };
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Mesh, meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* SkateMesh;
+
 public:
 	// Sets default values for this character's properties
 	ASkateCharacter();
+
 
 protected:
 	// Called when the game starts or when spawned
